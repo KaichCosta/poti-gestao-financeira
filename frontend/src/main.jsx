60b08b0a71a5a796f78d1ed3e7be9252cd08bb60
Kaich Costa <kaichhc@gmail.com>
2026-08-19 20:53:21 -0300

    adicionei vercel speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.js';
 import GlobalStyle from './styles/global.js';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>,
 )
